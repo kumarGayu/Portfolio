f5d6281ca03b66689a4a9aa3cfce6034234e49ef
Kumar Jayaram Gayatri <kumarjayaram.gayatri@mavs.uta.edu>
2017-04-08 02:09:06 -0500

    corrected port requirements for uploading app

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
+app.set('port', (process.env.PORT || 7777));
 
 app.use(express.static(path.join(__dirname,"../app/dist")));
-app.listen((process.env.PORT || 7777),function(){
-    console.log("Started listening on port", (process.env.PORT || 7777));
+app.listen(app.get('port'),function(){
+    console.log("Started listening on port", app.get('port'));
 })
