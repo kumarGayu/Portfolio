4c8e5db0d3150314296cf005f41525529b160ef9
Kumar Jayaram Gayatri <kumarjayaram.gayatri@mavs.uta.edu>
2017-04-08 02:08:27 -0500

    corrected non touched screen

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-app.set('port', (process.env.PORT || 7777));
 
 app.use(express.static(path.join(__dirname,"../app/dist")));
-app.listen(app.get('port'),function(){
-    console.log("Started listening on port", app.get('port'));
+app.listen((process.env.PORT || 7777),function(){
+    console.log("Started listening on port", (process.env.PORT || 7777));
 })
